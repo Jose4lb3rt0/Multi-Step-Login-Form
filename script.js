1d424f108271582344583b196c2e04114d615e9c
José Salcedo <josealberto3200@gmail.com>
2024-03-18 23:19:02 -0500

    Cambio de modulo registrar-iniciar-contraseña

--- script.js
+++ script.js
@@ -1,35 +1,73 @@
+//Pagina general
 const pageBox = document.querySelector('.page-box');
-const btnNext = document.querySelector('.btn-next');
-const btnBack = document.querySelector('.btn-back');
-const checkboxPass = document.querySelector('.checkbox-pass');
-const passwordInput = document.querySelector('.password');
-const loginTitle = document.querySelector('.loginTitle-text');
-const userEmail = document.querySelector('.user-email');
-const emailInput = document.querySelector('.email');
+
+
+    //Texto del titulo general
+    const Title = document.querySelector('.Title-text');
+    //Texto del correo para cambiar mediante interacción
+    const Subtitle = document.querySelector('.Subtitle-text');
+
+
+    //Variable que captura el input para ingresar el corro para iniciar sesión
+    const InputEmail = document.querySelector('.correo-ingresar');
+    //Botón para desplegar la creación de cuenta
+    const btnCrearCuenta = document.querySelector('.btn-crear-cuenta');
+    //Botón Next para ir a la contraseña
+    const btnNext = document.querySelector('.btn-next');
+
+
+    //Variable que captura el input para ingresar la contraseña para iniciar sesión
+    const InputPassword = document.querySelector('.contraseña-ingresar');
+    //Botón Back para retroceder de la contraseña hacia el correo
+    const btnBack = document.querySelector('.btn-back');
+    //Checkbox para enseñar contraseña
+    const checkboxPass = document.querySelector('.checkbox-contraseña');
+    //Botón Back para iniciar sesión
+    const btnIniciarSesion = document.querySelector('.btn-iniciar-sesion');
+    
+
+    const InputNombreRegistrar = document.querySelector('.nombre-registrar');
+    const InputApellidoRegistrar = document.querySelector('.apellido-registrar');
+    const InputEmailRegistrar = document.querySelector('.email-registrar');
+    const InputContraseñaRegistrar = document.querySelector('.contraseña-registrar');
+    const btnRegistrar = document.querySelector('.btn-registrar');
+
+
+//Avanzar a la pagina para registrar
+btnCrearCuenta.onclick = (e)=>{
+    e.preventDefault();
+    pageBox.classList.add('active-regi');
+    setTimeout(() => InputNombreRegistrar.focus(), 500);
+    Title.innerHTML='Registrate';
+    Subtitle.innerHTML='¿No tienes una cuenta?';
+}
+
+
 
 //Avanzar al clickear Siguiente
 btnNext.onclick = (e) => {
     e.preventDefault();
     pageBox.classList.add('active-pass');
     setTimeout(() => passwordInput.focus(), 500);
-    loginTitle.innerHTML = 'Bienvenido';
-    userEmail.innerHTML = emailInput.value;
+    Title.innerHTML = 'Bienvenido';
+    Subtitle.innerHTML = InputEmail.value;
 };
 
 //Retroceder al clickear Atrás
 btnBack.onclick = (e) => {
     e.preventDefault();
     pageBox.classList.remove('active-pass')
-    loginTitle.innerHTML = 'Inicia sesión';
-    userEmail.innerHTML = 'Por favor inicia sesión para usar la plataforma';
-    emailInput.focus()
+    Title.innerHTML = 'Inicia sesión';
+    Subtitle.innerHTML = 'Por favor inicia sesión para usar la plataforma';
+    InputEmail.focus()
 };
 
 //Mostrar contraseña checkbox
 checkboxPass.onclick = () => {
     if(checkboxPass.checked) {
-        passwordInput.type = 'text';
+        InputPassword.type = 'text';
     }else{
-        passwordInput.type = 'password';    
+        InputPassword.type = 'password';    
     }
 };
+
